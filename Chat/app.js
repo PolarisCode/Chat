04cd952dfca7e4a3e02ca481fd545fed917b1e8b
Imran Aliyev <imran.aliyev@gmail.com>
2015-12-06 23:10:07 +0400

    sending message to server has been implemented.

--- Chat/app.js
+++ Chat/app.js
@@ -1,10 +1,14 @@
-﻿var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+﻿var express = require('express');
+var http = require('http');
+var socketio = require('socket.io');
 var logger = require('morgan');
 
+var app = express();
+var httpServer = http.Server(app);
+var io = socketio(httpServer);
 
 app.use(logger('dev'));
+app.use(express.static('public'));
 
 
 app.get('/', function (req, res) {
@@ -13,11 +17,16 @@ app.get('/', function (req, res) {
 
 io.on('connection', function (socket) {
     console.log('a user connected');
+    
+    socket.on('chat message', function (msg) {
+        console.log('message: ' + msg);
+    });
+
     socket.on('disconnect', function () {
         console.log('user disconnected');
     });
 });
 
-http.listen(3000, function () {
+httpServer.listen(3000, function () {
     console.log('listening on *:3000');
 });
