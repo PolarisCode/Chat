c8c2233b890e0ae89f7a55f58fa855ff079a5213
Imran Aliyev <imran.aliyev@gmail.com>
2015-12-18 11:30:02 +0400

    user name input has been implemented.

--- Chat/app.js
+++ Chat/app.js
@@ -30,7 +30,7 @@ io.on('connection', function (socket) {
     
     socket.on('chat message', function (msg) {
         console.log('message: ' + msg);
-        io.emit('chat message', msg);
+        io.emit('chat message', currentUser + ": "+  msg);
     });
     
     socket.on('disconnect', function () {
--- Chat/public/client.js
+++ Chat/public/client.js
@@ -1,30 +1,37 @@
-﻿var socket = io();
-
-socket.emit('userEnter', new Date());
-
-$('form').submit(function () {
-    socket.emit('chat message', $('#m').val());
-    $('#m').val('');
-    return false;
-});
-
-function normalizeUsername(username){
-    return username.replace(/-/g, '').replace(/:/g, '').replace(/\./g, '')
-}
-
-socket.on('chat message', function (msg) {
-    $('#messages').append($('<li>').text(msg));
-});
-
-socket.on('userEnter', function (users) {
-    if (users) {
-        $('#users').empty();
-        for (var user in users) {
-            $('#users').append($('<li>').text(users[user]).attr('id', normalizeUsername(users[user])));
-        }
+﻿(function () {
+    var socket = io(),
+        form = $('form'),
+        messageList = $('#messages'),
+        userList = $('#users');
+    
+    var person = prompt("Please enter your name");
+    
+    socket.emit('userEnter', person);
+    
+    form.submit(function () {
+        socket.emit('chat message', $('#m').val());
+        $('#m').val('');
+        return false;
+    });
+    
+    function normalizeUsername(username) {
+        return username.replace(/-/g, '').replace(/:/g, '').replace(/\./g, '');
     }
-});
-
-socket.on('userExit', function (username) {
-    $('#users').find('#' + normalizeUsername(username)).remove();
-});
+    
+    socket.on('chat message', function (msg) {
+        messageList.append($('<li>').text(msg));
+    });
+    
+    socket.on('userEnter', function (users) {
+        if (users) {
+            userList.empty();
+            for (var user in users) {
+                userList.append($('<li>').text(users[user]).attr('id', normalizeUsername(users[user])));
+            }
+        }
+    });
+    
+    socket.on('userExit', function (username) {
+        userList.find('#' + normalizeUsername(username)).remove();
+    });
+})();
